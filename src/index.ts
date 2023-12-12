07774b31d5d313f9b892f58c0795724c69095692
woo3145 <lcwoo3145@gmail.com>
2023-12-12 19:47:48 +0900

    README 수정

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-/**
- * Copyright (c) 2023 ZEP Co., LTD
- */
+import { add } from './utils/add';
 
-import {add} from "./utils/add";
-
-ScriptApp.showCenterLabel("Hello World");
+ScriptApp.showCenterLabel('Hello World');
 ScriptApp.showCenterLabel(`1 + 2 = ${add(1, 2)}`);
 
-let zepLogo = ScriptApp.loadSpritesheet("zep_logo.png");
+let zepLogo = ScriptApp.loadSpritesheet('zep_logo.png');
 
 ScriptMap.putObject(0, 0, zepLogo, { overlap: true });
 
