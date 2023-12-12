0bd5ae2a7a4ea86225a7e63da89ff642f704c377
woo3145 <lcwoo3145@gmail.com>
2023-12-12 21:09:46 +0900

    widget.html 추가, 빌드에러 수정

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,46 @@
-import { add } from './utils/add';
+/**
+ * Copyright (c) 2023 ZEP Co., LTD
+ */
 
-ScriptApp.showCenterLabel('Hello World');
-ScriptApp.showCenterLabel(`1 + 2 = ${add(1, 2)}`);
+import { ScriptPlayer } from 'zep-script';
 
-let zepLogo = ScriptApp.loadSpritesheet('zep_logo.png');
+function openWidget(p: ScriptPlayer) {
+  if (p.isMobile)
+    //Set the widget's name, anchor position, width, and height.
+    p.tag.widget = p.showWidget('widget.html', 'sidebar', 300, 550);
+  else p.tag.widget = p.showWidget('widget.html', 'sidebar', 280, 520);
 
-ScriptMap.putObject(0, 0, zepLogo, { overlap: true });
+  //Send data to widget
+  p.tag.widget.sendMessage({
+    isMobile: p.isMobile,
+  });
+
+  //Register events for messages sent from widgets
+  p.tag.widget.onMessage.Add(function (sender: any, msg: any) {
+    switch (msg.type) {
+      case 'close':
+        if (p.tag.widget) {
+          p.tag.widget.destroy();
+          p.tag.widget = null;
+        }
+
+        break;
+      case 'submit':
+        break;
+    }
+  });
+}
 
 ScriptApp.onDestroy.Add(function () {
   ScriptMap.clearAllObjects();
+});
+
+ScriptApp.onJoinPlayer.Add(function (p) {
+  //Reset the player's tag value
+  p.tag = {};
+});
+
+// Action when the player clicks the sidebar app
+ScriptApp.onSidebarTouched.Add(function (p) {
+  openWidget(p);
 });
